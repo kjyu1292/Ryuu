ebdc40bf608727b7df5fd0a6d7601b318c829ba8
Kame Yoko <kjyu1292@gmail.com>
2026-03-06 18:13:17 +0800

    New test function

--- test/unit/prng/random_choice_noshuffle.c
+++ test/unit/prng/random_choice_noshuffle.c
@@ -7,7 +7,13 @@
 
 int main(int argc, char* argv[]) {
 
-	assert(argc >= 3);
+	if (argc < 3)
+	{
+		printf("Expect 3 arguments, passed %d.\n", argc);
+	} else 
+	{
+		for (int i = 0; i < argc; i++) { printf("%s\n", argv[i]); }
+	}
 
         int num_generations = atoi(argv[1]);
 	assert(num_generations % 16 == 0);
@@ -18,13 +24,12 @@ int main(int argc, char* argv[]) {
         struct timespec ts;
         clock_gettime(CLOCK_REALTIME, &ts);
         unsigned int seeds[16] __attribute__((aligned(64)));
-        unsigned int seed = (unsigned int) \
-                (ts.tv_sec*1000000000+ts.tv_nsec);
+        unsigned int seed = (unsigned int) (ts.tv_sec*1000000000+ts.tv_nsec);
 	seed = seed;
     
         for (register unsigned int i = 0; i < 4; i++)
         {
-                seeds[i] = 19650218; // + (seed & i) + (seed >> i);
+                seeds[i] = 19650218 + (seed & i) + (seed >> i);
         }    
  
         state__m512 stack_state;
@@ -38,32 +43,36 @@ int main(int argc, char* argv[]) {
                 64, (num_generations * sizeof(int) + 63) & ~63 
         );
 	_random_uint32__m512(st_ptr, idx_ptr, pre_computed_generations, num_blocks_m512);
+	for (register int i = 0; i < 4; i++)
+	{
+		printf("pre_computed_generations[%d]: %10u\n", i, pre_computed_generations[i]);
+	}
+	for (register int i = num_generations-4; i < num_generations; i++)
+	{
+		printf("pre_computed_generations[%d]: %10u\n", i, pre_computed_generations[i]);
+	}
 
 	long pdf_padded = (pdf_actual + 15) & ~15;
 	float pdf[(int) pdf_padded] __attribute__((aligned(64)));
 	register int _dummy = (int) pdf_actual;
-	for (register int i = 0; i < (int) pdf_actual; i++) 
-	{
-		pdf[i] = (float) 1/_dummy; 
-	}
+	for (register int i = 0; i < (int) pdf_padded; i++) { pdf[i] = 0.0f; }
+	for (register int i = 0; i < (int) pdf_actual; i++) { pdf[i] = (float) 1/_dummy; }
 	long pdf_info = pdf_actual + (pdf_padded << 32);
 
 	long res_actual = pdf_actual;
 	long res_padded = (res_actual + 15) & ~15;
 	float res[(int) res_padded] __attribute__((aligned(64)));
-	for (register int i = 0; i < (int) res_padded; i++) 
-	{
-		res[i] = 0.0f; 
-	}
+	for (register int i = 0; i < (int) res_padded; i++) { res[i] = 0.0f; }
 	long res_info = res_actual + (res_padded << 32);
 	assert(pdf_info == res_info);
+
 	_random_choice_noshuffle(
 		pre_computed_generations
 		, &(pdf[0]), pdf_info
 		, &(res[0]), res_info
 	);
 
-	for (register int i = 0; i < (int) res_actual; i++)
+	for (register int i = 0; i < (int) res_padded; i++)
 	{
 		printf(
 			"pdf[%2d] = %.7f | res[%2d] = %.7f\n"
